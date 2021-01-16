334b8e44f10d8e2652b70e3b319d301445f1f6e7
Artem Tumanov <artem_tuman@mail.ru>
2021-01-16 21:33:23 +0300

    Added animation

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
         <div className="title">
             <h1>ArtGram</h1>
             <h2>Your Pictures</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            <p>Click on the plus button to upload your picture. It will be saved in Firebase and available online.</p>
         </div>
     )
 }
